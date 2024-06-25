0b023a4f06f176425fccd43ff9d80782bbbb97aa
Ezequiel Lino <sonisombra@gmail.com>
2024-06-25 14:21:44 -0600

    creando rutas protegidas en node - middleware

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 import { registrar, perfil, confirmar, autenticar } from "../controllers/veterinarioController.js";
+import checkAuth from "../middleware/authMiddleware.js";
 
 router.post("/", registrar );
 
-router.get("/perfil", perfil)
+router.get("/perfil", checkAuth, perfil)
 
 router.get("/confirmar/:token", confirmar);
 
