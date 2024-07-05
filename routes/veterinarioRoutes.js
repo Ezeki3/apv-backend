013c6938927b3ae3a6dbab3b56437b1594a47e63
Ezequiel Lino <sonisombra@gmail.com>
2024-07-04 23:48:24 -0600

    comprobar si el token es valido

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -16,8 +16,8 @@ router.post("/", registrar );
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
 router.post("/olvide-password", olvidePassword);
-router.get("/olvide-password/:token", comprobarToken);
-router.post("/olvide-password/:token", nuevoPassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+// router.post("/olvide-password/:token", nuevoPassword);
 
 // área privada
 router.get("/perfil", checkAuth, perfil)
