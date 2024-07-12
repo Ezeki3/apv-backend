256c2bba0493aef2e3e0fe8e5f6bf897338c92ca
Ezequiel Lino <sonisombra@gmail.com>
2024-07-11 23:59:37 -0600

    almacenando el veterinario en el registro de pacientes

--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { agregarPaciente, obtenerPacientes } from '../controllers/PacienteController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 router.route('/')
-  .post(agregarPaciente)
+  .post(checkAuth, agregarPaciente)
   .get(obtenerPacientes)
 
 export default router;
