7a589b44908c95bd04011e7f35d089ca9b7ddc42
Ezequiel Lino <sonisombra@gmail.com>
2024-08-26 23:49:16 -0600

    enviando el email

--- herlpers/emailRegistro.js
+++ herlpers/emailRegistro.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
-const emailRegistro = () => {
-  var transport = nodemailer.createTransport({
+const emailRegistro = async (datos) => {
+  var transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
@@ -9,4 +9,25 @@ const emailRegistro = () => {
       pass: process.env.EMAIL_PASS
     }
   });
+
+  const { email, nombre, token } = datos;
+
+  const info = await transporter.sendMail({
+    from: "APV - Administrador de Pacientes de Veterinaria",
+    to: email,
+    subject: "Comprueba tu cuenta en APV",
+    text: 'Comprueba tu cuenta en APV',
+    html: `
+      <p>Hola: ${nombre}, comprueba tu cuenta en APV.</p>
+      <p>Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace:
+        <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobrar Cuenta</a>
+      </p>
+
+      <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p>
+    `
+  })
+
+  console.log('mensaje enviado: %s', info.messageId)
 }
+
+export default emailRegistro;
