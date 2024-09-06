d6d10b7a9950f3a8b5c601c69248e5e0f1a7597f
Ezequiel Lino <sonisombra@gmail.com>
2024-09-06 00:17:44 -0600

    primeros pasos con olvide password

--- herlpers/emailOlvidePassword.js
+++ herlpers/emailOlvidePassword.js
@@ -20,7 +20,7 @@ const emailOlvidePassword = async (datos) => {
     html: `
       <p>Hola: ${nombre}, has solicitado reestablecer tu password.</p>
       <p>Sigue el siguiente enlace para generar tu nuevo password:
-        <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Reestablecer Password</a>
+        <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Password</a>
       </p>
 
       <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p>
