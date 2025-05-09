831a907502c5871accb7e95a3fe90752e596dbc9
Ezequiel Lino <sonisombra@gmail.com>
2025-05-08 23:51:03 -0600

    comprobar que el password actual es correcto

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -190,8 +190,29 @@ const actualizarPerfil = async (req, res) => {
 };
 
 const actualizarPassword = async (req, res) => {
-  console.log(req.veterinario);
-  console.log(req.body);
+  // Leer los datos
+  const { id } = req.veterinario;
+  const { pwd_actual, pwd_nuevo } = req.body;
+
+  // Comprobar que el veterinario existe
+  const veterinario = await Veterinario.findById(id);
+  if (!veterinario) {
+    const error = new Error('Hubo un error');
+    return res.status(400).json({msg: error.message});
+  }
+
+  // Comprobar su password
+  if( await veterinario.comprobarPassword(pwd_actual)){
+    // Almacenar el nuevo password
+
+    veterinario.password = pwd_nuevo;
+    await veterinario.save();
+    res.json({msg:'Password almacenado correctamente'});
+
+  } else {
+    const error = new Error('El Password actual es incorrecto');
+    return res.status(400).json({msg: error.message});
+  }
   
   
 }
